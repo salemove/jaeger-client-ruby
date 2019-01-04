299ad0bc4ec355aa71d3031c8db7437e7b012991
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2019-01-04 09:11:10 -0500

    Use named constants for B3 headers instead of array for readability

--- lib/jaeger/extractors.rb
+++ lib/jaeger/extractors.rb
@@ -63,39 +63,39 @@ module Jaeger
     end
 
     class B3RackCodec
-      KEYS = %w[
-        HTTP_X_B3_TRACEID
-        HTTP_X_B3_SPANID
-        HTTP_X_B3_PARENTSPANID
-        HTTP_X_B3_FLAGS
-        HTTP_X_B3_SAMPLED
-      ].freeze
+      class Keys
+        TRACE_ID = 'HTTP_X_B3_TRACEID'.freeze
+        SPAN_ID = 'HTTP_X_B3_SPANID'.freeze
+        PARENT_SPAN_ID = 'HTTP_X_B3_PARENTSPANID'.freeze
+        FLAGS = 'HTTP_X_B3_FLAGS'.freeze
+        SAMPLED = 'HTTP_X_B3_SAMPLED'.freeze
+      end.freeze
 
       def self.extract(carrier)
-        B3CodecCommon.extract(carrier, KEYS)
+        B3CodecCommon.extract(carrier, Keys)
       end
     end
 
     class B3TextMapCodec
-      KEYS = %w[
-        x-b3-traceid
-        x-b3-spanid
-        x-b3-parentspanid
-        x-b3-flags
-        x-b3-sampled
-      ].freeze
+      class KEYS
+        TRACE_ID = 'x-b3-traceid'.freeze
+        SPAN_ID = 'x-b3-spanid'.freeze
+        PARENT_SPAN_ID = 'x-b3-parentspanid'.freeze
+        FLAGS = 'x-b3-flags'.freeze
+        SAMPLED = 'x-b3-sampled'.freeze
+      end.freeze
 
       def self.extract(carrier)
-        B3CodecCommon.extract(carrier, KEYS)
+        B3CodecCommon.extract(carrier, Keys)
       end
     end
 
     class B3CodecCommon
       def self.extract(carrier, keys)
-        trace_id = TraceId.base16_hex_id_to_uint64(carrier[keys[0]])
-        span_id = TraceId.base16_hex_id_to_uint64(carrier[keys[1]])
-        parent_id = TraceId.base16_hex_id_to_uint64(carrier[keys[2]])
-        flags = parse_flags(carrier[keys[3]], carrier[keys[4]])
+        trace_id = TraceId.base16_hex_id_to_uint64(carrier[keys::TRACE_ID])
+        span_id = TraceId.base16_hex_id_to_uint64(carrier[keys::SPAN_ID])
+        parent_id = TraceId.base16_hex_id_to_uint64(carrier[keys::PARENT_SPAN_ID])
+        flags = parse_flags(carrier[keys::FLAGS], carrier[keys::SAMPLED])
 
         return nil if span_id.nil? || trace_id.nil?
         return nil if span_id.zero? || trace_id.zero?
